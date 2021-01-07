8b007da46d18d41562f388de312f5a3d1d0a9508
alexandre <amenezes031@gmail.com>
2021-01-07 12:06:42 -0300

    Lifecycle react component Hello

--- front-web/src/components/Hello.tsx
+++ front-web/src/components/Hello.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type Props = {
   message: String;
 }
 
 function Hello({ message }: Props) {
+
+  useEffect(() => {
+    console.log("Iniciando componente Hello.")
+  }, []);
+
   return (
     <h1>Hello { message }!</h1>
   );
