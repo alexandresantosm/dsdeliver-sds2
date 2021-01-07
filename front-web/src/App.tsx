0c65b07ba386e16639aa878b6028cc79e2f7d54e
alexandre <amenezes031@gmail.com>
2021-01-07 12:30:09 -0300

    Revert  Add state react component Counter

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Hello from './components/Hello';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <>
-    <Hello message="Alexandre" />
-    <Hello message="Senama DevSuperior 2" />
-    <Hello message="DevSuperior" />
+    <Counter />
     </>
   );
 }
