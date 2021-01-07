fc827c27849a4f09b4d0abf0392b1f8228f75b2b
alexandre <amenezes031@gmail.com>
2021-01-07 14:28:43 -0300

    Created component Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
